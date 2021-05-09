f2d479d99bff1979d61dc6fdd58802c61981cf73
tranecheg <yarchinko@yandex.ru>
2021-05-09 14:03:55 +0300

    fix

--- Assets/finish/Scripts/Shooting.cs
+++ Assets/finish/Scripts/Shooting.cs
@@ -20,7 +20,7 @@ public class Shooting : MonoBehaviourPun
 
     private float fireRate;
     private float fireTimer = 0.0f;
-    private bool useLaser;
+    public bool useLaser;
     public LineRenderer lineRenderer;
     public bool isShooting;
     public Joystick joystick;
